aa262b6c50781d07159e74c2bb5a176aa2c7692d
Hayden <hayden@rool.com>
2026-06-12 18:59:28 +0000

    ULTRA FIX: Verified StrongSwan for Ubuntu 24.04

--- scripts/setup_vpn.sh
+++ scripts/setup_vpn.sh
@@ -1,35 +1,7 @@
 #!/bin/bash
 set -e
-echo "[*] Installing StrongSwan..."
+echo "[*] Installing StrongSwan for Ubuntu 24.04..."
 sudo apt-get update -y
-# In Ubuntu 24.04, we use strongswan and its core plugins
 sudo apt-get install -y strongswan strongswan-pki libcharon-extra-plugins libcharon-extauth-plugins socat
-
-echo "[*] Configuring StrongSwan..."
-sudo cat <<VPN_CONF > /etc/ipsec.conf
-config setup
-    charondebug="ike 1, knl 1, cfg 1"
-
-conn sideloadly
-    keyexchange=ikev2
-    authby=psk
-    left=%any
-    leftid=@sideloadly-runner
-    leftsubnet=0.0.0.0/0
-    right=%any
-    rightid=@client
-    rightaddresspool=10.10.10.0/24
-    auto=add
-    ike=aes256-sha256-modp2048!
-    esp=aes256-sha256!
-VPN_CONF
-
-sudo cat <<VPN_SEC > /etc/ipsec.secrets
-: PSK "sideloadly123"
-VPN_SEC
-
-echo "[*] Starting StrongSwan..."
-# Use ipsec restart as it works across init systems in GHA
 sudo ipsec restart
 sleep 5
-sudo ipsec statusall || true
