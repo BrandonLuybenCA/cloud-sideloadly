da2d125a7ea73d50c2190f92bb2f415fcf6f4093
Hayden <hayden@rool.com>
2026-06-12 17:31:06 +0000

    Update VPN to PSK for easier manual setup

--- scripts/setup_vpn.sh
+++ scripts/setup_vpn.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+# IKEv2 Setup with PSK (Pre-Shared Key)
 sudo apt-get update && sudo apt-get install -y strongswan libcharon-extra-plugins
+
+# Configure VPN with PSK
 sudo cat <<VPN_CONF > /etc/ipsec.conf
 config setup
-    charondebug="ike 2, knl 2, cfg 2"
+    charondebug="ike 1, knl 1, cfg 1"
+
 conn sideloadly
     keyexchange=ikev2
-    ike=aes256-sha256-modp2048!
-    esp=aes256-sha256!
     left=%any
-    leftauth=pubkey
     leftid=@sideloadly-runner
     leftsubnet=10.10.10.0/24
     right=%any
-    rightauth=pubkey
-    rightid=%any
+    rightid=@client
     rightsourceip=10.10.10.0/24
+    authby=secret
     auto=add
 VPN_CONF
+
+# Set the Shared Secret
+sudo bash -c 'echo "@sideloadly-runner @client : PSK \"sideloadly123\"" > /etc/ipsec.secrets'
+
+# Start VPN
 sudo ipsec restart
-echo "[*] VPN Server is Live."
+echo "[*] VPN Server is Live with PSK: sideloadly123"
